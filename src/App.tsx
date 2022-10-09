f8840f4b8ed816423ecddf44155425e448ee4d6f
Martin Slanina <martin.slanina1995@gmail.com>
2022-10-09 20:34:01 +0200

    ci: add predeploy and deploy script

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ function App() {
                                     <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
                                 </svg>
                             </div>
-                            <input type="search" id="default-search"
+                            <input type="search" id="default-search" autoComplete="off"
                                    className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                    placeholder="eg. 662731289533143168" onInput={onTextInput}/>
                         </div>
